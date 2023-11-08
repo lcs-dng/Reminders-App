203bd498c8f3b6a8a17c82692ffdb36f43458888
Dexter Ng <DNg@lcs.on.ca>
2023-11-08 08:38:47 -0500

    Initial Commit

---
+++ Reminders App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reminders App
+//
+//  Created by Dexter Ng on 2023-11-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reminders App/Reminders_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reminders_AppApp.swift
+//  Reminders App
+//
+//  Created by Dexter Ng on 2023-11-08.
+//
+
+import SwiftUI
+
+@main
+struct Reminders_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
